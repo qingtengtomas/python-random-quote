49fecc97745390328f7a23aefb71124fce722091
kaeriny <kaeriny@kali-linux.localdomain>
2021-04-28 16:48:28 +0800

    Renamed the primary function

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
+import random
 def shulo():
-   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last=13
+  rnd=random.randint(0,last)
 
-  #print(quotes)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   shulo()
